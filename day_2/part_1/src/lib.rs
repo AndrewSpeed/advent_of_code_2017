57e62de3762f06938115e56b90fa97f299c47bf7
Andrew Speed <andrew.cspeed@gmail.com>
2017-12-07 00:18:54 +0000

    Add calculate_difference function for day 2 part 1 with passing tests

--- day_2/part_1/src/lib.rs
+++ day_2/part_1/src/lib.rs
@@ -1,7 +1,38 @@
+fn calculate_checksum(input: &str) -> i32 {
+    return 0;
+}
+
+fn calculate_difference(values: Vec<i32>) -> i32 {
+    let mut sorted_values = values.clone();
+    sorted_values.sort();
+    let smalled_value = sorted_values.first().unwrap();
+    let largest_value = sorted_values.last().unwrap();
+
+    return largest_value - smalled_value;
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn calculate_difference_can_find_difference_with_largest_beside_smallest() {
+        assert_eq!(calculate_difference(vec![5, 1, 9, 5]), 8);
+    }
+
+    #[test]
+    fn calculate_difference_can_find_difference_with_largest_before_smallest() {
+        assert_eq!(calculate_difference(vec![7, 5, 3]), 4);
+    }
+
+    #[test]
+    fn calculate_difference_can_find_difference_with_largest_after_smallest() {
+        assert_eq!(calculate_difference(vec![2, 4, 6, 8]), 6);
+    }
+
+    #[test]
+    fn calculate_checksum_works_correctly() {
+        let input = "5 1 9 5\n7 5 3\n2 4 6 8";
+        assert_eq!(calculate_checksum(input), 18);
     }
 }
